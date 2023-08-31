c1179838946f170daf430e8252ef7d06a35fb8da
andreitoledo <andreiltoledo@hotmail.com>
2023-08-31 12:00:39 -0300

    Imprementando o Element-Dialog

---
+++ src/app/shared/element-dialog/element-dialog.component.ts
@@ -0,0 +1,25 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import {MatDialog, MAT_DIALOG_DATA, MatDialogRef, MatDialogModule} from '@angular/material/dialog';
+import { PeriodicElement } from 'src/app/views/home/home.component';
+
+@Component({
+  selector: 'app-element-dialog',
+  templateUrl: './element-dialog.component.html',
+  styleUrls: ['./element-dialog.component.scss']
+})
+export class ElementDialogComponent implements OnInit{
+  element!: PeriodicElement;
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+    public data: PeriodicElement,
+    public dialogRef: MatDialogRef<ElementDialogComponent>,
+  ) {}
+
+  ngOnInit(): void {
+  }
+
+  onCancel(): void {
+    this.dialogRef.close();
+  }
+}
